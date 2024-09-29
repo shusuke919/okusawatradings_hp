6e6c2b48818074160f5fef53c731083e9acdf7b8
shusuke919 <sh091965@gmail.com>
2024-09-29 19:52:18 +0900

    SEO対策

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>
+          {typeof metadata.title === "string"
+            ? metadata.title
+            : "奥沢トレーディングス"}
+        </title>
+        <meta
+          name="description"
+          content={
+            typeof metadata.description === "string"
+              ? metadata.description
+              : "奥沢トレーディングスはweb制作事業、webアプリ事業に特化した会社です。"
+          }
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "奥沢トレーディングス",
+              url: "", // ここに会社のURLを記入
+              logo: "", // ここにロゴのURLを記入
+              contactPoint: {
+                "@type": "ContactPoint",
+                areaServed: "JP",
+                availableLanguage: ["Japanese"],
+              },
+            }),
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
